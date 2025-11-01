0642e65097bc16197c7d9d989dc9b84ff7a62c20
A1exFox <alex.fox.personal@gmail.com>
2025-11-01 20:40:52 +0300

    035 sharing data with template

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/a1exfox/go-course/pkg/config"
+	"github.com/a1exfox/go-course/pkg/models"
 	"github.com/a1exfox/go-course/pkg/render"
 )
 
@@ -24,8 +25,12 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
